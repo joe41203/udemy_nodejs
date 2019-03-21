3e8aa85098e13581813240bfc97ef92c6ea33498
joe <kinjoyuji@gmail.com>
2019-03-21 15:11:14 +1100

    render smaple html

--- 1_sample/server.js
+++ 1_sample/server.js
@@ -1,14 +1,56 @@
 const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const mineTypes = {
+  html: 'text/html',
+  jpeg: 'image/jpeg',
+  jpg: 'image/jpg',
+  png: 'image/png',
+  js: 'text/javascript',
+  css: 'text/css'
+};
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+http
+  .createServer((req, res) => {
+    var uri = url.parse(req.url).pathname;
+    var filename = path.join(process.cwd(), unescape(uri));
+    console.log('Loading' + uri);
+    var stats;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+    try {
+      stats = fs.lstatSync(filename);
+    } catch (e) {
+      res.writeHead(404, { 'Content-type': 'text/plain' });
+      res.write('404 not found \n');
+      res.end();
+      return;
+    }
+
+    if (stats.isFile()) {
+      var mineType =
+        mineTypes[
+          path
+            .extname(filename)
+            .split('.')
+            .reverse()[0]
+        ];
+
+      res.writeHead(200, { 'Content-type': mineType });
+
+      var fileStream = fs.createReadStream(filename);
+      fileStream.pipe(res);
+    } else if (stats.isDirectory()) {
+      res.writeHead(302, {
+        Location: 'index.html'
+      });
+
+      res.end();
+    } else {
+      res.writeHead(500, { 'Content-type': 'text/plain' });
+      res.write('500 error internal error');
+      res.end();
+    }
+  })
+  .listen(1337);
